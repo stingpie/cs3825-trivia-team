a8ba7855afd066e5a086dba68f60a04ab2556b8b
Gabriel Wright <stingpie52@gmail.com>
2026-07-27 14:45:26 -0500

    basic structure. added dataserver and basic flask server.

---
+++ server.py
@@ -0,0 +1,76 @@
+from flask import Flask, session, request, jsonify
+from multiprocessing.managers import BaseManager
+
+app= Flask(__name__)
+
+trivia_data_server=BaseManager(('localhost', 5555))
+trivia_data_server.register('get_trivia')
+trivia_data_server.register('verify_answer')
+trivia_data_server.register('new_trivia_set')
+trivia_data_server.register('register_user')
+
+@app.route("/")
+def hello_world():
+    return "<p> hello, world</p>"
+
+@app.route("api/trivia", method=["GET"])
+def get_trivia():
+    if('UUID' in session and 'idx_of_trivia_set' in session and 'question_idx' in session):
+        result = trivia_data_server.get_trivia(session['idx_of_trivia_set'], session['question_idx'])
+        if(result):
+            return jsonify(result)
+        else:
+            return "Error in getting the next question.", 500
+    if('UUID' not in session):
+        return "User not logged in.", 401 # unauthorized
+    if('idx_of_trivia_set' not in session):
+        return "No active trivia set selected.", 400
+    if('question_idx' not in session):
+        return "No question index found", 400
+        
+@app.route("api/trivia/verify", method=["GET"])
+def verify_answer(): # should recieve {'answer':'something seomthing something'} returns {'correct':true|false}
+    answer = request.get_json()['question']
+    if('UUID' in session and 'idx_of_trivia_set' in session and 'question_idx' in session):
+        response={}
+        response['correct']=trivia_data_server.verify_answer(session['idx_of_trivia_set'], session['question_idx'])
+        return jsonify(response)
+    if('UUID' not in session):
+        return "User not logged in.", 401 # unauthorized
+    if('idx_of_trivia_set' not in session):
+        return "No active trivia set selected.", 400
+    if('question_idx' not in session):
+        return "No question index found", 400
+
+@app.route("api/trivia/next", method=["GET"])
+def next_question():
+    if('UUID' in session and 'idx_of_trivia_set' in session and 'question_idx' in session):
+        session['question_idx']+=1
+        result = get_trivia()
+        if(isinstance(result, tuple) and result[1]<300):
+            return result
+        elif(isinstance(result, tuple) and result[1]>=300):
+            pass # TODO: this is probably the end of the trivia quiz.
+        else:
+            return result
+            
+
+
+@app.route("api/users", method=['POST'])
+def create_user(): # should recieve {'username':"asldjad", "password":"alsdjoa"} returns 201 on success
+    user_info = request.get_json()
+    username= user_info['username']
+    password = user_info['password']
+    result = trivia_data_server.register_user(username, password)
+    if isinstance(result, str):
+        return result, 409
+    session['UUID']=result
+    session['idx_of_trivia_set']=0
+    session['question_idx']=0
+    return 'new user created!', 201
+
+
+
+
+
+
---
+++ trivia-manager.py
@@ -0,0 +1,109 @@
+from multiprocessing import Lock
+from multiprocessing.managers import BaseManager
+import atexit
+import pickle
+from uuid import UUID
+
+trivia_data_server = BaseManager(('localhost', 5555))
+trivia_set_lock=Lock()
+users_lock=Lock()
+
+
+
+
+# trivia json specification:
+# [
+#  {'type':'multiple select',
+#   'question':'what are the factors of 6?',
+#   'possible responses': ['12', '2', '4', '3'],
+#   'correct answers':['2','3']
+#  },
+#  {'type':'short answer',
+#   'question': 'Who was the first president of the US?',
+#   'correct answers':['George Washington', 'President George Washington']
+#  },
+#  {'type':'multiple choice',
+#  'question': 'What is the role of RAM in a computer?',
+#  'possible responses': ['long term storage of information', 'processing math', 'producing audio effects', 'fast temporary storage'],
+#  'correct answers':['fast temporary storage']
+#  }
+# ]
+
+class TriviaSet():
+    def __init__(self, trivia_specification):
+        self.trivia=trivia_specification
+
+    def verify_answer(self, index, answer_to_test): # TODO: answer normalization for short answer questions
+        return answer_to_test in self.trivia[index]['correct_answers'] 
+
+    def get_question(self, index):
+        ret = self.trivia[index].copy()
+        ret['correct_answers']=None    # prevent the client from scraping the password.
+        return ret
+
+
+
+
+class User():
+    def __init__(self, username, password, UUID):
+        self.username=username
+        self.password=password
+        self.UUID=UUID 
+        
+
+
+
+
+def start(): # TODO: make this secure. include a passcode with every trivia set that only authorized users have. 
+             # TODO: Make this list into a dictionary, indexed with a UUID. then, associate a unique lock with every trivia set.
+             # TODO: load trivia sets from file, if file exists. 
+    global trivia_sets 
+    global users
+    users={}
+    trivia_sets=[]
+
+
+def get_trivia(idx_of_trivia_set, question_idx): # TODO: make this secure. Include a passcode with every trivia set that only authorized users have.
+    with trivia_set_lock:
+        if(question_idx>=trivia_sets[idx_of_trivia_set]):
+            return False
+        return trivia_sets[idx_of_trivia_set].get_question(question_idx)
+
+def verify_answer(idx_of_trivia_set, question_idx, answer):
+    with trivia_set_lock:
+        return trivia_sets[idx_of_trivia_set].verify_answer(question_idx, answer)
+
+def new_trivia_set(trivia_json):
+    with trivia_set_lock:
+        trivia_sets+=[TriviaSet(trivia_json)]
+        return len(trivia_sets)-1
+
+def register_user(username, password):
+    with users_lock:
+        if any(map(lambda x: x.username==username), users.values()):
+            return "username already taken"
+        new_uuid = UUID4()
+        users[new_uuid]=[User(username, password, new_uuid)]
+        return uuid
+
+
+
+@atexit.register
+def goodbye(): #TODO: save trivia sets and users.
+    pass
+
+
+trivia_data_server.register('get_trivia', get_trivia)
+trivia_data_server.register('verify_answer', verify_answer)
+trivia_data_server.register('new_trivia_set', new_trivia_set)
+trivia_data_server.register('register_user', register_user)
+
+server= trivia_data_server.get_server()
+server.serve_forever()
+
+
+
+
+
+
+
